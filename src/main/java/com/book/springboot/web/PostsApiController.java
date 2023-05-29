aaa1fb17fb9d0cc969143d2bf601e0ee410c0459
sohyeonnn <74013080+sohyeonnn@users.noreply.github.com>
2023-05-29 16:22:44 +0900

    feat: 게시글 등록, 수정, 조회 기능 추가

---
+++ src/main/java/com/book/springboot/web/PostsApiController.java
@@ -0,0 +1,33 @@
+package com.book.springboot.web;
+
+import com.book.springboot.service.posts.PostsService;
+import com.book.springboot.web.dto.PostsResponseDto;
+import com.book.springboot.web.dto.PostsSaveRequestDto;
+import com.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    //등록(작성)
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    //수정
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    //조회
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
