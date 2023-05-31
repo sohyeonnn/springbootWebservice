b3d6bfa705d47885486c30eee5c43d2145f2a1ce
sohyeonnn <74013080+sohyeonnn@users.noreply.github.com>
2023-05-31 18:04:28 +0900

    feat: 게시글 전체 조회

--- src/main/java/com/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/book/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,13 @@
 package com.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+    
+    //게시글 전체 조회
+    @Query("select p from Posts p order by p.id desc")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/book/springboot/web/IndexController.java
+++ src/main/java/com/book/springboot/web/IndexController.java
@@ -1,12 +1,16 @@
 package com.book.springboot.web;
 
+import com.book.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
+
+    private final PostsService postsService;
 
     /*
     @GetMapping("/")
@@ -15,8 +19,16 @@ public class IndexController {
     }
      */
 
+    //게시글 작성
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    //게시글 전체 조회
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
     }
 }
