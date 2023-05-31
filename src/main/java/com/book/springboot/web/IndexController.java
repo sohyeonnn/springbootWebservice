72b8c185897a6f752f7e4de8861b87aee6d2c353
sohyeonnn <74013080+sohyeonnn@users.noreply.github.com>
2023-05-31 18:23:37 +0900

    feat: 게시글 수정 화면 작성, controller 연결

--- src/main/java/com/book/springboot/web/IndexController.java
+++ src/main/java/com/book/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.book.springboot.web;
 
 import com.book.springboot.service.posts.PostsService;
+import com.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
@@ -30,5 +32,14 @@ public class IndexController {
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
         return "index";
+    }
+
+    //게시글 수정
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
